d128fdab9daf8aba986cae035b5655f368802252
Jen Pirrone <pirronej104@gmail.com>
2018-11-17 17:45:09 -0500

    Saturday Evening commit

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,12 +1,11 @@
 //array of available words 
 
-var availableWords = ["pizza", "spaghetti", "sandwich", "bagel", "taco", "burger"];
+var availableWords = ["pizza", "Popcorn", "sandwich", "cookie", "taco", "burger"];
 var currentWord = [];
 var currentWordIndex;
-var maxGuesses = 10;
 var lettersGuessed = [];
 var remainingLives = 0;
-var gameStarted = true;
+var gameStarted = false;
 var gameOver = true;
 var wins = 0;
 
@@ -15,7 +14,7 @@ var wins = 0;
 //Start game
 
 function startGame () {
-    remainingLives = maxGuesses;
+    remainingLives = 10;
     gameStarted = true;
 
 
@@ -79,14 +78,7 @@ function findLetter(letter) {
         }
     }
 };
-function checkWin() {
-    if(currentWord.indexOf(letter) === -1) {
-        // document.getElementById("youwin-image").style.cssText = "display: block";
-        // document.getElementById("pressKeyTryAgain").style.cssText= "display: block";
-        wins++;
-        gameOver = true;
-    }
-};
+
 function Guess(letter) {
     if (remainingLives > 0) {
 
@@ -100,6 +92,19 @@ function Guess(letter) {
     updateGame();
 
 };
+function checkWin() {
+    if(currentWord.indexOf("_") === -1) {
+        // document.getElementById("youwin-image").style.cssText = "display: block";
+        // document.getElementById("pressKeyTryAgain").style.cssText= "display: block";
+        wins++;
+        gameOver = true;
+    }
+    else if (remainingLives == 0) {
+        gameOver = true;
+
+    }
+};
+
 
 // This function takes a letter and finds all instances of 
 // appearance in the string and replaces them in the guess word.
@@ -108,41 +113,10 @@ function Guess(letter) {
     // if there are no indicies, remove a guess and update the hangman image
    
 document.onkeydown = function(event) {
-    // If we finished a game, dump one keystroke and reset.
-    if(gameOver) {
-        startGame();
-        gameOver = false;
-    } else {
         // Check to make sure a-z was pressed.
         if(event.keyCode >= 65 && event.keyCode <= 90) {
             Guess(event.key.toLowerCase());
             updateGame();
         }
-    }
+    
 };
-//repeat the following until the chosen word is completed or lives equals 0
-
-
-
-//Listen to the user's keypress
-
-//search an alphabet array for a match
-
-//return the selected letter 
-
-//search the chosen word for the letter 
-
-//if the letter is in the chosen word, search the word for letter position
-
-//display the letter in place of the empty space
-
-//if the letter is not in the chosen word, print the letter to the screen below the game 
-
-//decrease lives by 1
-
-//if lives equals 0, display, You Lose!
-
-//if word is completed, display, You Win! 
-
-//reset counter and word if user presses 'play again' 
-
