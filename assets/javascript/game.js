850236c0e23888e4392c5a23f15849dc110ef6a9
Jen Pirrone <pirronej104@gmail.com>
2018-11-17 23:30:56 -0500

    Final Saturday commit

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -20,18 +20,18 @@ function startGame () {
     wins = 0;
 
 
-//Choose a word for the game from given array 
+//Choose a word at random from the array of available words
 
 currentWordIndex = Math.floor(Math.random() * (availableWords.length));
 
-//reset the game
+//Reset Game Arrays and Images
 lettersGuessed = [];
 currentWord = [];
 
 document.getElementById("winimage").style.cssText = "display: none";
 document.getElementById("loseimage").style.cssText = "display: none";
 
-//Print spaces to the page (one for every letter in the word)
+//Print blank spaces to the page
 for (var i=0; i < availableWords[currentWordIndex].length; i++) {
     currentWord.push("_");
 }
@@ -39,10 +39,10 @@ for (var i=0; i < availableWords[currentWordIndex].length; i++) {
 updateGame();
 };
 
-
+//Print the gameplay to the display page
 function updateGame() {
 
-    document.getElementById("totalWins").innerText = wins;
+    // document.getElementById("totalWins").innerText = wins;
     var currentWordString = "";
     for (var i = 0; i < currentWord.length; i++) {
         currentWordString += currentWord[i];
@@ -51,31 +51,30 @@ function updateGame() {
     document.getElementById("currentWord").innerText = currentWordString;
     document.getElementById("remainingLives").innerText = remainingLives;
     document.getElementById("lettersGuessed").innerText = lettersGuessed;
-    // if(remainingLives <= 0) {
-    //     document.getElementById("gameover-image").style.cssText = "display: block";
-    //     document.getElementById("pressKeyTryAgain").style.cssText = "display:block";
-    //     gameOver = true;
-    // }
+
 };
 
+//Find the letter in the current word
 function findLetter(letter) {
-    // Array to store letterPosition of letters in string
+    // Array used to store the letters for the game
     var letterPosition = [];
 
-    // Loop through word finding all instances of guessed letter, store the indicies in an array.
+    // Loop through the current word and push the indices into the letterPosition array 
     for (var i = 0; i < availableWords[currentWordIndex].length; i++) {
         if(availableWords[currentWordIndex][i] === letter) {
             letterPosition.push(i);
         }
     }
 
+    //Decrement remaining lives if the selected letter does not find a match
     if (letterPosition.length <= 0) {
         remainingLives--;
         
     } else {
-        // Loop through all the indicies and replace the '_' with a letter.
+        // Loop through letters, replace the spaces with the letter if a match exists
         for(var i = 0; i < letterPosition.length; i++) {
             currentWord[letterPosition[i]] = letter;
+            //increment correct guesses counter
             wins++;
             console.log(wins);
         }
@@ -98,42 +97,43 @@ function Guess(letter) {
 
 };
 function checkWin() {
+    //Evaluate wins counter and word length to look for a match
     if(currentWord.length === wins ) {
+        //Put the win image on the screen
         document.getElementById("winimage").style.cssText = "display: block";
-        
-        // wins = 1;
         gameOver = true;
         
     }
+    //Evaluate remaining lives counter to see if a loss exists
     else if (remainingLives == 0) {
+        console.log(availableWords[currentWordIndex]);
+        //alert the solution so the user sees who 'defeated' them
+        alert(availableWords[currentWordIndex] + " defeted you!");
+        //Put the lose image on the screen
         document.getElementById("loseimage").style.cssText = "display: block";
         gameOver = true;
-
     }
-    
-    
+
 };
 
 
 
 
-// This function takes a letter and finds all instances of 
-// appearance in the string and replaces them in the guess word.
-
-
-    // if there are no indicies, remove a guess and update the hangman image
+ // Evaluate keypresses
    
 document.onkeydown = function(event) {
     
 
     
-        // Check to make sure a-z was pressed.
+        // Listen for A-Z
         if(event.keyCode >= 65 && event.keyCode <= 90 && gameOver !== true) {
             Guess(event.key.toLowerCase());
             updateGame();
             checkWin();
             console.log(gameOver);
         }
+
+        // Lisetn for 'Enter' key
 
         if(event.keyCode == 13) {
             startGame();
